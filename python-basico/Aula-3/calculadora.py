25500c0e4c65759ca1f561771acbca33e375e1c5
Pedro Henrique <correiapedro208@gmail.com>
2026-07-02 17:44:19 -0300

    Feat: Aula3

---
+++ python-basico/Aula-3/calculadora.py
@@ -0,0 +1,13 @@
+def somar(a, b):
+    return a + b
+
+def multiplicar(a, b):
+    return a * b
+
+if __name__ == '__main__':
+    
+
+    resultado_soma = somar(10, 5)
+    resultado_multiplicacao = multiplicar(4, 3)
+    print(f'O resultado da soma é: {resultado_soma}')
+    print(f'O resultado da multiplicação é: {resultado_multiplicacao}')
---
+++ python-basico/Aula-3/main.py
@@ -0,0 +1,4 @@
+import calculadora 
+
+print(f"O resultado da soma é: {calculadora.somar(10, 5)}")
+print(f"O resultado da multiplicação é: {calculadora.multiplicar(4, 3)}")
